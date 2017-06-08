de18488e570adcaa0b4bd30ad7577d9b4a598d44
Karl Lam <karl.v.lam@gmail.com>
2017-06-08 18:19:18 +0800

    ok

--- 02-match.rs
+++ 02-match.rs
@@ -1,6 +1,6 @@
 fn main(){
     let numbers = [0,1,2,3,4,5,18,22,30,39,40,-41];
-    for number in &numbers { // 0 to 31
+    for number in &numbers { 
         let result = match *number {
             1 => "咪一囉！", // expression
             2|3|5|7 => "質數啲Friend!",
