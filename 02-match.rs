740971cb227a8b7bd415d3c8dde6600a98ab8b20
Karl Lam <karl.v.lam@gmail.com>
2017-06-08 18:18:13 +0800

    more use cases

--- 02-match.rs
+++ 02-match.rs
@@ -1,12 +1,18 @@
 fn main(){
-    let numbers = [0,1,2,3,4,5,18,22,30,39,40];
+    let numbers = [0,1,2,3,4,5,18,22,30,39,40,-41];
     for number in &numbers { // 0 to 31
         let result = match *number {
             1 => "咪一囉！", // expression
             2|3|5|7 => "質數啲Friend!",
             18|22 => "傳說中的十八、廿二",
             30...39 => "三張嘢……",
-            _ => "其他，係咁多", // uncomment and see...
+            // comment below and see...
+            _ => 
+                if number < &0 {
+                    "-這是負數-" 
+                }else{
+                    "+這是正數+" 
+                }
         };
 
         println!("{} -> {}", number, result);
